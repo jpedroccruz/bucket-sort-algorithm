06bc58e532e5dbaa768a45f7aca1dc767a85d9dd
jpedroccruz <joaopedroccruz.dev@gmail.com>
2025-10-04 19:51:36 -0300

    add js file that generate dataset

---
+++ get_dataset.js
@@ -0,0 +1,12 @@
+import { writeFileSync } from 'node:fs'
+import puppeteer from 'puppeteer'
+
+const browser = await puppeteer.launch()
+const page = await browser.newPage()
+await page.goto("https://www.dailymail.co.uk/news/article-2523930/Jesus-famous-person-history-according-software-algorithm.html", { waitUntil: 'networkidle2' })
+
+const famous_people = (await page.$$eval('p b', element => element.map(name => name.textContent))).reverse()
+
+await browser.close()
+
+writeFileSync('data.csv', famous_people.join('\n'), 'utf-8')
